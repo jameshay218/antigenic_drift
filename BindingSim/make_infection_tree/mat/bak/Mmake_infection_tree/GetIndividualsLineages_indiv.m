bc1852d43030aab55684f062130eab3c98cb13d2
hy39 <seanyuan0323@gmail.com>
2015-12-21 18:02:56 -0500

    remove backup file

--- BindingSim/make_infection_tree/mat/bak/Mmake_infection_tree/GetIndividualsLineages_indiv.m
+++
@@ -1,5 +0,0 @@
-function parentLineages = GetIndividualsLineages_indiv(n_seqs, indiv_sampled, parent)
-
-for i = 1:n_seqs
-    parentLineages(i).lin = GetSampleLineage(indiv_sampled(i), parent);
-end
