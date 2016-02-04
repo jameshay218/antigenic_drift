7919abbed971272b82df302eb6da19744338467e
james <jameshay218@gmail.com>
2016-02-04 12:35:35 +0000

    tmp

--- CppInfectionSim/driftSim/src/virus.cpp
+++ CppInfectionSim/driftSim/src/virus.cpp
@@ -162,8 +162,8 @@ double Virus::probSurvival(Host* _host){
   double dist = findDistanceToHost(_host);
   double immJ = (_host->get_hostK() - dist);
   if(immJ < 0) immJ = 0;
-  if(immJ/_r >= 1) prob = 0;
   double prob = pow((1-exp(-_p*(bindingavid + _q))),_r*immJ);
+  if(immJ/_r >= 1) prob = 0;
   return(prob);
 }
 
