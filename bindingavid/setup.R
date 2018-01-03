180af74e866ffd8a136f59900db9a7c526d076c5
Sean YY <seanyuan0323@gmail.com>
2018-01-03 14:35:35 +0800

    creat jobs folder

--- bindingavid/setup.R
+++ bindingavid/setup.R
@@ -1,22 +1,33 @@
-                                                                                                                                                                                                                                                                                                                    # setup environment
+# preprocess                                                                                                                                                                                                                                                                                                                    # setup environment
 rm(list = ls())
 library("Rcpp")
+library("reshape")
 
+## install driftSim
+#  from local disk
 #setwd("F:/Documents/GitHub/antigenic_drift/CppInfectionSim/driftSim")
 #devtools::load_all()
-library("reshape")
-# go to antigenic_drift/bindingavid subfolder
+## from Github
 setwd("F:/Documents/GitHub/antigenic_drift/bindingavid")
 devtools::install_github("jameshay218/antigenic_drift/CppInfectionSim/driftSim")
 library(driftSim)
-setwd("F:/Documents/GitHub/antigenic_drift/CppInfectionSim/driftSim")
-#devtools::load_all()qq
-setwd("F:/Documents/GitHub/antigenic_drift/bindingavid")
+packageVersion('driftSim')
 
-# start batch analysis
-# update the source folder
+## dependent files for batch analysis
 myhome <- "F:/Documents/GitHub/antigenic_drift/bindingavid"
+# Generate starting conditions
 source(paste(c(myhome,"/cluster_submission.R"),collapse = ''))
 source(paste(c(myhome,"/plot_SIR.R"),collapse = ''))
 source(paste(c(myhome,"/saveoutput.R"),collapse = ''))
-source(paste(c(myhome,"/bindingavid_jobs6_pc.R"),collapse = ''))
+
+# start batch analysis with different scenarios
+# (1)single_fixed_low, (2)single_fixed_high
+# (3)single_adaptive_low, (4)single_adaptive_high
+# (4.1) single_adaptive_high + different binding avidity 
+# (6)multiple peaks
+source(paste(c(myhome,"/jobs/bindingavid_jobs1_pc.R"),collapse = '')) #(1)
+# source(paste(c(myhome,"/jobs/bindingavid_jobs2_pc.R"),collapse = '')) #(2)
+# source(paste(c(myhome,"/jobs/bindingavid_jobs3_pc.R"),collapse = '')) #(3)
+# source(paste(c(myhome,"/jobs/bindingavid_jobs4_pc.R"),collapse = '')) #(4)
+# source(paste(c(myhome,"/jobs/bindingavid_jobs4_allbind_pc.R"),collapse = '')) #(4.1)
+# source(paste(c(myhome,"/jobs/bindingavid_jobs6_2_pc.R"),collapse = '')) #(6)
